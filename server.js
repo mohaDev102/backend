6c081ee4aeb765d48637e9709f4328490ccb320c
Mohamed <mohaelattar6@gmail.com>
2024-06-16 15:33:24 +0200

    mailer

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-// server.js
-require('dotenv').config();
-const express = require('express');
+// // server.js
+// require('dotenv').config();
+// // const express = require('express');
+// const nodemailer = require('nodemailer');
+// // const bodyParser = require('body-parser');
+// const cors = require('cors'); // Para permitir solicitudes CORS desde el frontend
+// // const app = express();
+// // const port = 3001;
+
+// // Middleware
+// // app.use(cors());
+// // app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// // Configuración de Nodemailer
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+// // Ruta para manejar el envío de correos
+// app.post('/send-email', (req, res) => {
+//   const { name, email, message } = req.body;
+//     console.log('ee');
+//   const mailOptions = {
+//     from: email,
+//     to: process.env.EMAIL_USER,
+//     subject: `Mensaje de ${name}`,
+//     text: message
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       console.log(error);
+//       return res.status(500).send('Error al enviar el correo');
+//     }
+//     console.log('Email enviado: ' + info.response);
+//     res.status(200).send('Correo enviado exitosamente');
+//   });
+// });
+
 const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Para permitir solicitudes CORS desde el frontend
-const app = express();
-const port = 3001;
+require('dotenv').config();
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Configuración de Nodemailer
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -21,28 +53,26 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// Ruta para manejar el envío de correos
-app.post('/send-email', (req, res) => {
-  const { name, email, message } = req.body;
-    console.log('ee');
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_USER,
-    subject: `Mensaje de ${name}`,
-    text: message
-  };
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      return res.status(500).send('Error al enviar el correo');
-    }
-    console.log('Email enviado: ' + info.response);
-    res.status(200).send('Correo enviado exitosamente');
-  });
-});
-console.log('eew');
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor funcionando en http://localhost:${port}`);
-});
+    const mailOptions = {
+      from: email,
+      to: 'mohaelattar6@gmail.com',
+      subject: `Mensaje de ${name}`,
+      text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error al enviar el correo:', error);
+        return res.status(500).send('Error al enviar el correo');
+      }
+      console.log('Email enviado: ' + info.response);
+      res.status(200).send('Correo enviado exitosamente');
+    });
+  } else {
+    res.status(405).send('Método no permitido');
+  }
+};
